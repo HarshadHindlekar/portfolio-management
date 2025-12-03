4dc7abf2e407d128bcbfd0c2eaed8f72a6d75714
HarshadHindlekar <harshad.hindlekar29@gmail.com>
2025-12-03 23:07:46 +0530

    Updates

---
+++ _next/static/RbKPDiIZo3cOC4iaEqTGJ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-management//_next/:path+",
+        "destination": "/portfolio-management/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
